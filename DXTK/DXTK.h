16cea2c81f920a02c76ee1e3e62ccf4e83082e35
CaueAnjos <kawidosanjos@outlook.com>
2023-09-04 10:48:23 -0300

    add config macros

--- DXTK/DXTK.h
+++ DXTK/DXTK.h
@@ -7,3 +7,18 @@
 #include "App.h"
 #include "Graphics.h"
 #include "Engine.h"
+
+#define START_CONFIG \
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)\
+{\
+    try\
+    {
+
+#define END_CONFIG \
+}\
+catch(const Error& error)\
+{\
+    MessageBox(nullptr, error.description().c_str(), "DXTK Error", MB_OK);\
+    return 0;\
+}\
+}
